beb47a2d47fef4cf4003a8ad6a733cba66c5db3c
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-02-13 18:42:19 -0500

    update

--- src/uhtf/schema.sql
+++ src/uhtf/schema.sql
@@ -1,7 +1,17 @@
 -- Initialize the database.
 -- Drop any existing data and create empty tables.
 
+DROP TABLE IF EXISTS instrument;
 DROP TABLE IF EXISTS part;
+
+CREATE TABLE instrument (
+    id INTEGER PRIMARY KEY,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT NULL,
+    description TEXT UNIQUE NOT NULL,
+    hostname TEXT UNIQUE NOT NULL,
+    port INTEGER DEFAULT 5025
+);
 
 CREATE TABLE part (
     id INTEGER PRIMARY KEY,
