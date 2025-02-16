206afda44d9ed4f58baa5038d69455412b9886ed
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-02-16 16:31:53 -0500

    update

--- src/uhtf/protocol.py
+++ src/uhtf/protocol.py
@@ -142,7 +142,7 @@ async def delete():
 
 
 @protocol.post("/protocol/update")
-async def update() -> tuple:
+async def update():
     """Update protocol endpoint."""
 
     form = (await request.form).copy().to_dict()
@@ -168,3 +168,38 @@ async def update() -> tuple:
     except db.IntegrityError:
         await flash("Invalid parameter(s).", "warning")
     return redirect(url_for(".read"))
+
+
+@protocol.post("/protocol/<int:part_id>")
+async def raw():
+    rows = get_db().execute(
+        """
+        SELECT
+            command.scpi AS scpi,
+            command.delay AS delay,
+            instrument.hostname AS hostname,
+            instrument.port AS port,
+            measurement.name AS measurement_name,
+            measurement.units AS units,
+            measurement.lower_limit AS lower_limit,
+            measurement.upper_limit AS upper_limit,
+            phase.name AS phase_name
+        FROM
+            protocol
+        INNER JOIN
+            command ON command.id = protocol.command_id
+        INNER JOIN
+            instrument ON instrument.id = protocol.instrument_id
+        OUTER LEFT JOIN
+            measurement ON measurement.id = protocol.measurement_id
+        INNER JOIN
+            part ON part.id = protocol.part_id
+        INNER JOIN
+            phase ON phase.id = protocol.phase_id
+        WHERE
+            part.id = ?
+        """,
+        (part_id,) 
+    ).fetchall()
+    records = list(map(dict, rows))
+    return records, 201
