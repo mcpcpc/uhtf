004f9773681c779802dc9ae30fef23862441bf2a
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-02-16 13:33:48 -0500

    update

--- src/uhtf/protocol.py
+++ src/uhtf/protocol.py
@@ -88,7 +88,8 @@ async def create() -> tuple:
     """Create protocol callback."""
 
     form = (await request.form).copy().to_dict() 
-    form["measurement_id"] = form.get("measurement_id")
+    #form["measurement_id"] = form.get("measurement_id")
+    print(form)
     try:
         db = get_db()
         db.execute("PRAGMA foreign_keys = ON")
