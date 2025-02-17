f6ae8d5e989c0c83b1676c007a458dda206ccd8f
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-02-16 20:07:57 -0500

    update

--- src/uhtf/models/processor.py
+++
@@ -1,8 +0,0 @@
-from itertools import groupby
-
-from .tcp import TCP
-
-def processor(records: list[dict]):
-    grouped = groupby(records, key="phase")
-    for name, phase in grouped.items():
-        
