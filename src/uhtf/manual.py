5199df1f2e177e67aa2e5ac201849f91ebce7fce
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-02-19 17:08:13 -0500

    update

---
+++ src/uhtf/manual.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+"""
+SPDX-FileCopyrightText: 2025 Michael Czigler
+SPDX-License-Identifier: BSD-3-Clause
+
+Manual test endpoints.
+"""
+
+from quart import Blueprint
+from quart import flash
+from quart import redirect
+from quart import render_template
+from quart import request
+from quart import url_for
+
+from .database import get_db
+
+manual = Blueprint("manual", __name__)
+
+
+@app.get("/single")
+def read():
+    phases = get_db().execute(
+        """
+        SELECT * FROM phase
+        """
+    ).fetchall()
+    return await render_template(
+        "manual.html",
+        phases=phases,
+    )
