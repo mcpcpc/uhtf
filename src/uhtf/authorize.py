1617e4c3967d2a37831a228f59b15390c166f854
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-02-20 21:00:57 -0500

    update

--- src/uhtf/authorize.py
+++ src/uhtf/authorize.py
@@ -9,10 +9,13 @@ Authorize endpoints.
 """
 
 from quart import Blueprint
+from quart import flash
 from quart import redirect
 from quart import render_template
+from quart import request
 from quart import session
 from quart import url_for
+from werkzeug.security import check_password_hash
 
 from .database import get_db
 
@@ -24,6 +27,28 @@ async def login() -> tuple:
     """Login callback."""
 
     return await render_template("login.html")
+
+
+@authorize.post("/authorize/login")
+async def validate() -> tuple:
+    """Validate callback."""
+
+    form = await request.form
+    password = get_db().execute(
+        """
+        SELECT password FROM setting WHERE id = 1
+        """
+    ).fetchone()
+    print(password)
+    if not isinstance(form.get("password"), str):
+        await flash("Missing password.", "warning")
+        return redirect(url_for(".login"))
+    if not check_password_hash(password, form["password"]):
+        await flash("Invalid password.", "warning")
+        return redirect(url_for(".login"))
+    session.clear()
+    session["user_id"] = user["id"]
+    return redirect(url_for("index"))
 
 
 @authorize.get("/authorize/logout")
